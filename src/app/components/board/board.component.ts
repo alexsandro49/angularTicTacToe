e81ae5a04376d5fbf2d4b698d8e5e0c28f189560
alexsandro49 <alexsandro49.dev@gmail.com>
2023-11-28 16:01:04 -0300

    implemented computer logic

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -11,18 +11,8 @@ import { ScoreboardService } from '../../services/scoreboard.service';
   styleUrl: './board.component.css',
 })
 export class BoardComponent implements OnInit {
-  squares: string[] = ['', '', '', '', '', '', '', '', ''];
-  squaresColor: string[] = [
-    '#285cc4',
-    '#285cc4',
-    '#285cc4',
-    '#285cc4',
-    '#285cc4',
-    '#285cc4',
-    '#285cc4',
-    '#285cc4',
-    '#285cc4',
-  ];
+  squares!: string[];
+  squaresColor!: string[];
   isPlayer!: boolean;
   playerValue!: string;
   computerValue!: string;
@@ -30,10 +20,10 @@ export class BoardComponent implements OnInit {
   color2!: string;
 
   constructor(private scoreBoardService: ScoreboardService) {
-    this.restart();
   }
 
   ngOnInit(): void {
+    this.restart();
     this.scoreBoardService.restart
       .asObservable()
       .subscribe(() => this.restart());
@@ -43,17 +33,12 @@ export class BoardComponent implements OnInit {
   }
 
   squareSelected(event: any): void {
-    if (this.squares[event.target.id] != '') {
+    if (this.squares[event.target.id] != '' || !this.isPlayer) {
       return;
     }
-
-    if (this.isPlayer) {
-      this.squares[event.target.id] = this.playerValue;
-      this.squaresColor[event.target.id] = this.color1;
-    } else {
-      this.squares[event.target.id] = this.computerValue;
-      this.squaresColor[event.target.id] = this.color2;
-    }
+    
+    this.squares[event.target.id] = this.playerValue;
+    this.squaresColor[event.target.id] = this.color1;
 
     if (this.championVerify()) {
       this.scoreBoardService.champion.next(
@@ -69,6 +54,12 @@ export class BoardComponent implements OnInit {
     this.scoreBoardService.currentPlayer.next(
       this.isPlayer ? 'PLAYER' : 'COMPUTER'
     );
+
+    setTimeout(() => {
+      if (!this.isPlayer) {
+          this.computerPlay();
+        }
+    }, 1000);
   }
 
   restart(): void {
@@ -100,6 +91,12 @@ export class BoardComponent implements OnInit {
     this.scoreBoardService.currentPlayer.next(
       this.isPlayer ? 'PLAYER' : 'COMPUTER'
     );
+
+    setTimeout(() => {
+      if (!this.isPlayer) {
+          this.computerPlay();
+        }
+    }, 1000);
   }
 
   championVerify(): boolean {
@@ -139,4 +136,102 @@ export class BoardComponent implements OnInit {
 
     return false;
   }
+
+  computerAttack(): boolean {
+    for (let i = 0; i < 3; i++) {
+      if (this.squares[i * 3] == '' && this.squares[1 + 3 * i] == this.computerValue && this.squares[2 + 3 * i] == this.computerValue) {
+        this.squares[i * 3] = this.computerValue; this.squaresColor[i * 3] = this.color2; return true;
+      } else if (this.squares[i * 3] == this.computerValue && this.squares[1 + 3 * i] == '' && this.squares[2 + 3 * i] == this.computerValue) {
+        this.squares[1 + i * 3] = this.computerValue; this.squaresColor[1 + i * 3] = this.color2; return true;
+      } else if (this.squares[i * 3] == this.computerValue && this.squares[1 + 3 * i] == this.computerValue && this.squares[2 + 3 * i] == '') {
+        this.squares[2 + 3 * i] = this.computerValue; this.squaresColor[2 + 3 * i] = this.color2; return true;
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (this.squares[i] == '' && this.squares[3 + i] == this.computerValue && this.squares[6 + i] == this.computerValue) {
+        this.squares[i] = this.computerValue; this.squaresColor[i] = this.color2; return true;
+      } else if (this.squares[i] == this.computerValue && this.squares[3 + i] == '' && this.squares[6 + i] == this.computerValue) {
+        this.squares[3 + i] = this.computerValue; this.squaresColor[3 + i] = this.color2; return true;
+      } else if (this.squares[i] == this.computerValue && this.squares[3 + i] == this.computerValue && this.squares[6 + i] == '') {
+        this.squares[6 + i] = this.computerValue; this.squaresColor[6 + i] = this.color2; return true;
+      }
+    }
+
+    for (let i = 0; i < 2; i++) {
+      if (this.squares[i + 2 * i] == '' && this.squares[4] == this.computerValue && this.squares[8 - 2 * i] == this.computerValue) {
+        this.squares[i + 2 * i] = this.computerValue; this.squaresColor[i + 2 * i] = this.color2; return true;
+      } else if (this.squares[i + 2 * i] == this.computerValue && this.squares[4] == '' && this.squares[8 - 2 * i] == this.computerValue) {
+        this.squares[4] = this.computerValue; this.squaresColor[4] = this.color2; return true;
+      } else if (this.squares[i + 2 * i] == this.computerValue && this.squares[4] == this.computerValue && this.squares[8 - 2 * i] == '') {
+        this.squares[8 - 2 * i] = this.computerValue; this.squaresColor[8 - 2 * i] = this.color2; return true;
+      }
+    }
+
+    return false;
+  }
+
+  computerDefense(): boolean {
+    for (let i = 0; i < 3; i++) {
+      if (this.squares[i * 3] == '' && this.squares[1 + 3 * i] == this.playerValue && this.squares[2 + 3 * i] == this.playerValue) {
+        this.squares[i * 3] = this.computerValue; this.squaresColor[i * 3] = this.color2; return true;
+      } else if (this.squares[i * 3] == this.playerValue && this.squares[1 + 3 * i] == '' && this.squares[2 + 3 * i] == this.playerValue) {
+        this.squares[1 + i * 3] = this.computerValue; this.squaresColor[1 + i * 3] = this.color2; return true;
+      } else if (this.squares[i * 3] == this.playerValue && this.squares[1 + 3 * i] == this.playerValue && this.squares[2 + 3 * i] == '') {
+        this.squares[2 + 3 * i] = this.computerValue; this.squaresColor[2 + 3 * i] = this.color2; return true;
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (this.squares[i] == '' && this.squares[3 + i] == this.playerValue && this.squares[6 + i] == this.playerValue) {
+        this.squares[i] = this.computerValue; this.squaresColor[i] = this.color2; return true;
+      } else if (this.squares[i] == this.playerValue && this.squares[3 + i] == '' && this.squares[6 + i] == this.playerValue) {
+        this.squares[3 + i] = this.computerValue; this.squaresColor[3 + i] = this.color2; return true;
+      } else if (this.squares[i] == this.playerValue && this.squares[3 + i] == this.playerValue && this.squares[6 + i] == '') {
+        this.squares[6 + i] = this.computerValue; this.squaresColor[6 + i] = this.color2; return true;
+      }
+    }
+
+    for (let i = 0; i < 2; i++) {
+      if (this.squares[i + 2 * i] == '' && this.squares[4] == this.playerValue && this.squares[8 - 2 * i] == this.playerValue) {
+        this.squares[i + 2 * i] = this.computerValue; this.squaresColor[i + 2 * i] = this.color2; return true;
+      } else if (this.squares[i + 2 * i] == this.playerValue && this.squares[4] == '' && this.squares[8 - 2 * i] == this.playerValue) {
+        this.squares[4] = this.computerValue; this.squaresColor[4] = this.color2; return true;
+      } else if (this.squares[i + 2 * i] == this.playerValue && this.squares[4] == this.playerValue && this.squares[8 - 2 * i] == '') {
+        this.squares[8 - 2 * i] = this.computerValue; this.squaresColor[8 - 2 * i] = this.color2; return true;
+      }
+    }
+
+    return false;
+  }
+
+  computerPlay(): void {
+    if (!this.computerAttack()) {
+      if(!this.computerDefense()) {
+        let squarePosition: number = Math.floor(Math.random() * 9);
+          while (this.squares[squarePosition] != '') {
+            squarePosition = Math.floor(Math.random() * 9);
+          }
+
+          this.squares[squarePosition] = this.computerValue;
+          this.squaresColor[squarePosition] = this.color2;
+      }
+    }
+
+    if (this.championVerify()) {
+      this.scoreBoardService.champion.next(
+        this.isPlayer ? 'PLAYER' : 'COMPUTER'
+      );
+      return;
+    } else if (this.squares.every((x) => x != '')) {
+      this.scoreBoardService.champion.next('DRAW');
+      return;
+    }
+
+    this.isPlayer = !this.isPlayer;
+    this.scoreBoardService.currentPlayer.next(
+      this.isPlayer ? 'PLAYER' : 'COMPUTER'
+    );
+  }
 }
+
